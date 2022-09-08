6b596f2c7ae7a765caebe3872c6e1831bd56a7c6
Carol <carolina.jpeg@gmail.com>
2022-09-08 18:47:33 -0300

    requisito 4

---
+++ src/components/Category.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { getCategories } from '../services/api';
+
+export default class Category extends Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  fetchApi = async () => {
+    const categories = await getCategories();
+    this.setState({ categories });
+  };
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map((elemento) => (
+        //   <p key={ elemento.id }>
+        //     {elemento.name}
+        //   </p>
+          <section key={ elemento.id }>
+            <label htmlFor={ elemento.id }>
+              <input
+                name="categories"
+                type="radio"
+                id={ elemento.id }
+                data-testid="category"
+              />
+              {elemento.name}
+            </label>
+          </section>
+        ))}
+      </div>
+    );
+  }
+}
