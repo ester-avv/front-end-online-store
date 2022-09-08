ca925ea4102647dcef99b6a412b450bab38b6708
Thierry <varelathierry@gmail.com>
2022-09-08 16:05:55 -0300

    transformando o componente app em class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div> </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div> </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
